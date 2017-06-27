0c5d0838ef3c8a24273657fa7c7d6aa96509f590
Maria Mata <mariajo.mata@gmail.com>
2017-06-26 19:17:14 -0600

    Added ingredient list to card reveal content

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
   var appKey = "0a17582107ea8f662ffaf8279e8731fa"
   var range = "&from=0&to=12" // 12 results for testing
 
-  // "enter" key event listener
-  $("#go-btn").click(function() {
+  // Need to fix to allow multiple searches
+  $("form.search-form").submit(function(event) {
+    event.preventDefault()
     var query = "?" + "q=" + $(".main-search").val().replace(/ /g, ",").replace(/,,/g, ",")
     var requestURL = baseURL + query + range
     var settings = {
@@ -20,35 +21,43 @@ $(document).ready(function() {
       }
     }
     $.ajax(settings).then(appendRecipeCards);
-    })
   })
+
+}) // end of $(document).ready
+
+function ingredientList(array) {
+  var list = $("<ul></ul>")
+  for (var i = 0; i < array.length; i++) {
+    // need to add li bullets
+    list.append("<li style='list-style-type: disc'>" + array[i])
+  }
+  return list
+}
 
 function createNewCard(parent, recipe, index) {
   var card = $('<div class="card col s12 m3"></div>')
-  var cardImage = $('<div id=' + index + ' class="card-image waves-effect waves-block waves-light"></div')
+  var cardImage = $('<div id=' + index +
+    ' class="card-image waves-effect waves-block waves-light"></div')
   var cardContent = $("<div id=" + index + " class='card-content'></div>")
   var cardReveal = $("<div id=" + index + " class='card-reveal'></div")
-
-  $(card).append(cardImage) //img container
-  $(card).append(cardContent) // contentContainer
-  $(card).append(cardReveal) // reveal container
-  $(cardImage).append($('<img class="activator" src="' + recipe.image + '">')) // card image
-  $(cardContent).append($("<span class='card-title activator grey-text text-darken-4'>" + recipe.label + "</span>")) // card Title
-  $(cardContent).append($("<p><a class='link' href=" + recipe.url + "target='_blank'></a></p>")) // card link
-  $(cardReveal).append($("<span class='card-title grey-text text-darken-4'>" + recipe.label + "</span>")) // reveal Title
-  $(cardReveal).append($("<p></p>"))
-
+  $(card).append(cardImage)
+  $(card).append(cardContent)
+  $(card).append(cardReveal)
+  $(cardImage).append($('<img class="activator" src="' + recipe.image + '">'))
+  // Need to fix title spacing
+  $(cardContent).append($("<span class='card-title activator grey-text text-darken-4'>"
+    + recipe.label + "<i class='material-icons right'>more_vert</i></span>"))
+  $(cardContent).append($("<p><a class='link' target='_blank' href="
+    + recipe.url + ">See Full Details</a></p>"))
+  // Need to fix title spacing
+  $(cardReveal).append($("<span class='card-title grey-text text-darken-4'>"
+    + recipe.label + "<i class='material-icons right'>close</i></span>"))
+  $(cardReveal).append(ingredientList(recipe.ingredientLines))
   return $(parent).append(card)
 }
 
 function appendRecipeCards(data) {
   for (var i = 0; i < data.hits.length; i++) {
-    createNewCard($("#search-results"), data.hits[i].recipe, i) // creates new card
-    // data.hits[i].recipe.image
-    // data.hits[i].recipe.label // Title
-    // data.hits[i].recipe.ingredientLines // array of ingredients
-        // create a function that array to list
+    createNewCard($("#search-results"), data.hits[i].recipe, i)
   }
 }
-
-// before appending cards, set display to none for default content
